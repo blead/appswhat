62e77d88869418de033e10a15d1e4c541f1d1472
blead <blead010@gmail.com>
2018-04-19 21:19:12 +0700

    Implement express app

--- logger/index.js
+++ logger/index.js
@@ -7,11 +7,32 @@ const config = require('./config')
 const client = new AppsWhatClient(config.mqtt.path, config.mqtt.clientId)
 
 client.on('connect', function() {
-  client.subscribe('+')
-  client.publish('test', {message: 'Hello World!'})
+  client.subscribe('#')
 })
 
 client.on('message', function(packet) {
   console.log(`topic: ${packet.topic}`.concat(packet.retain ? ' (retained)' : ''))
   console.log(packet.payload)
+})
+
+app.use(cors())
+
+app.get('/message', function(req, res) {
+  if(!req.query.start || !req.query.end) {
+    res.sendStatus(400)
+  }
+  res.json([
+    {
+      id: req.query.start,
+      message: 'start :D',
+    },
+    {
+      id: req.query.end,
+      message: 'end :D',
+    },
+  ])
+})
+
+app.listen(config.port, function() {
+  console.log(`listening on port ${config.port}`)
 })
