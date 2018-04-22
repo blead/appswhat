eada0af2de79e6d4d49b4bfa8c81a796c9dfd92a
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-23 00:47:36 +0700

    add bootstrap-vue and relevant webpack configs

--- client/src/components/main.vue
+++ client/src/components/main.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <p> Hello </p>
+    <b-container>
+      <b-row>
+        <b-col>
+          <p> Hello </p>
+        </b-col>
+        <b-col>
+          <p> Bootstrap </p>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import bContainer from 'bootstrap-vue/es/components/layout/container'
+import bRow from 'bootstrap-vue/es/components/layout/row'
+import bCol from 'bootstrap-vue/es/components/layout/col'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    'b-container': bContainer,
+    'b-row': bRow,
+    'b-col': bCol
+  }
 }
 </script>
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,19 @@
 import { AppsWhatClient } from '@shared/'
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Main from './components/main'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-const client = new AppsWhatClient('http://localhost:80', 'peaw')
-console.log(client)
-client.on('message', console.log)
-client.subscribe('test')
-client.publish('test', 'hello')
-console.log('hello from webpack')
+// const client = new AppsWhatClient('http://localhost:80', 'peaw')
+// console.log(client)
+// client.on('message', console.log)
+// client.subscribe('test')
+// client.publish('test', 'hello')
+// console.log('hello from webpack')
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 let app = new Vue({
   el: '#app',
