a9d370e4e70c765dadb9196a68b64825a3436f37
peawyoyoyin <peawyoyoyin@gmail.com>
2018-04-23 13:44:09 +0700

    install and create blank vue-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import { AppsWhatClient } from '@shared/'
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-import Main from './components/main'
+import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import router from './router'
+
+import Main from './components/main'
 // const client = new AppsWhatClient('http://localhost:80', 'peaw')
 // console.log(client)
 // client.on('message', console.log)
@@ -14,10 +17,12 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 /* eslint-disable no-new */
 let app = new Vue({
   el: '#app',
   components: { Main },
+  router,
   render(el) {
     return el('Main')
   }
