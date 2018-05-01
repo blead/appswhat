1226668017c00a172fdacbda6e855637c77255b5
blead <blead010@gmail.com>
2018-05-01 18:11:10 +0700

    Improve NewChatModal, OK button is working, autofocus input

--- client/src/components/sidebar/chat-list/new-chat-modal.vue
+++ client/src/components/sidebar/chat-list/new-chat-modal.vue
@@ -1,11 +1,11 @@
 <template>
-  <b-modal :id="this.modalID" title="New Chat" name="name" ref="modal">
+  <b-modal :id="this.modalID" title="New Chat" name="name" ref="modal" @shown="this.onShown">
     <b-form id="newchat" @submit="this.onNewChat" autocomplete="off">
-      <b-form-input placeholder="enter topic" name="topic"></b-form-input>
+      <b-form-input placeholder="enter topic" name="topic" ref="topicInput" v-model="topic"></b-form-input>
     </b-form>
     <template slot="modal-footer">
       <b-button @click="this.closeModal">Cancel</b-button>
-      <b-button variant="primary" type="submit" form="newchat">OK</b-button>
+      <b-button variant="primary" type="submit" form="newchat" @click="this.onNewChat">OK</b-button>
     </template>
   </b-modal>
 </template>
@@ -14,11 +14,21 @@
 export default {
   name: 'NewChatModal',
   props: ['modalID'],
+  data() {
+    return {
+      topic: '',
+    }
+  },
   methods: {
+    onOk() {
+      alert(this.topic)
+    },
+    onShown(e) {
+      this.$refs.topicInput.focus()
+    },
     onNewChat(event) {
       event.preventDefault()
-      const topic = event.target.topic.value
-      console.log(topic)
+      const topic = this.topic
       this.$emit('newChat', topic)
       this.closeModal()
     },
