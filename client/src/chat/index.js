41320d6fb472170aa034c5c06007c9cf7df119a3
blead <blead010@gmail.com>
2018-05-01 16:54:13 +0700

    Fix paths for docker-compose

--- client/src/chat/index.js
+++ client/src/chat/index.js
@@ -1,6 +1,6 @@
 import { AppsWhatClient } from '@shared'
 
-const _LOCATION = 'http://load-balancer:8008'
+const _LOCATION = 'http://localhost:8008'
 let _client = null
 class ChatClient {
   static login(clientID) {
--- load-balancer/index.js
+++ load-balancer/index.js
@@ -10,7 +10,7 @@ const config = require('./config')
 const servers = config.servers.map(function(server) {
   return {
     url: server.url,
-    connection: mqtt.connect(server.url),
+    connection: mqtt.connect(server.internalPath),
     load: null,
     poolItem: null,
   }
