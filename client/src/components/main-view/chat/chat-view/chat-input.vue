24bf7cbce8ba338379c70cb1f2c4d297aaf20eac
peawyoyoyin <peawyoyoyin@gmail.com>
2018-05-04 13:41:23 +0700

    remove useless logs

--- client/src/components/main-view/chat/chat-view/chat-input.vue
+++ client/src/components/main-view/chat/chat-view/chat-input.vue
@@ -35,7 +35,6 @@ export default {
     },
     onYoda() {
       this.$emit('yoda')
-      console.log('yoda', this.yoda)
     }
   },
   components: {
--- client/src/components/main.vue
+++ client/src/components/main.vue
@@ -125,7 +125,6 @@ export default {
       })
     },
     onYoda() {
-      console.log('toggle yoda')
       this.yoda = !this.yoda
     },
     onUserLogout() {
